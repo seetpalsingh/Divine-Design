a9b08a92b98e3792ec71d87f6cc002f3eef9f547
Seetpal singh <creators.guru@gmail.com>
2013-06-01 22:49:29 -0700

    free portfolio template from codemode.in
    enjoy the template free for porrtfolio.

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+$(document).ready(function(){	
+	$("#slider").easySlider({
+/*		auto: true,
+		continuous: true,
+		speed:1400
+		
+		*/
+		speed: 			1000,
+		auto:			true,
+		pause:			4000,
+		continuous:		true
+	});
+});	
